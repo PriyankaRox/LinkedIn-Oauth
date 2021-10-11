7210672e7c57286f43b7e37d97fc1225719d43d0
Priyanka <priyankavrokhade@gmail.com>
2021-10-11 12:30:39 +0530

    My project

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'linkedinAuth': {
+      'clientID': '8666wp2dq9q38d', // your App ID
+      'clientSecret': 'Ej8zitT0DdTOaoOW', // your App Secret
+      'callbackURL': 'http://127.0.0.1:3000/auth/linkedin/callback'
+    }
+  }
